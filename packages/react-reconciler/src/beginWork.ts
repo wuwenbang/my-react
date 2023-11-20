c7fbb344ed122107fab459b84bbb2ca00170e730
wuwenbang <wuwenbang@foxmail.com>
2023-11-20 16:16:18 +0800

    feat: beginWork stage

--- packages/react-reconciler/src/beginWork.ts
+++ packages/react-reconciler/src/beginWork.ts
@@ -1,7 +1,54 @@
+import { ReactElementType } from 'shared/ReactTypes';
+import { mountChildFibers, reconcileChildFibers } from './childFibers';
 import { FiberNode } from './fiber';
+import { processUpdateQueue, UpdateQueue } from './updateQueue';
+import { HostComponent, HostRoot, HostText } from './workTags';
 
 // 递归中的递阶段
-export const beginWork = (fiber: FiberNode) => {
+export const beginWork = (wip: FiberNode) => {
 	// 比较，返回子fiberNode
-	return fiber;
+	switch (wip.tag) {
+		case HostRoot:
+			return updateHostRoot(wip);
+		case HostComponent:
+			return updateHostComponent(wip);
+		case HostText:
+			return null;
+		default:
+			if (__DEV__) {
+				console.warn('beginWork未实现的类型');
+			}
+			break;
+	}
+};
+
+const updateHostRoot = (wip: FiberNode) => {
+	const baseState = wip.memorizedState;
+	const updateQueue = wip.updateQueue as UpdateQueue<Element>;
+	const pending = updateQueue.shared.pending;
+	updateQueue.shared.pending = null;
+	const { memorizedState } = processUpdateQueue(baseState, pending);
+	wip.memorizedState = memorizedState;
+	const nextChildren = wip.memorizedState;
+	reconcileChildren(wip, nextChildren);
+	return wip.child;
+};
+
+const updateHostComponent = (wip: FiberNode) => {
+	const nextProps = wip.pendingProps;
+	const nextChildren = nextProps.children;
+	reconcileChildren(wip, nextChildren);
+	return wip.child;
+};
+
+const reconcileChildren = (wip: FiberNode, children?: ReactElementType) => {
+	const current = wip.alternate;
+
+	if (current !== null) {
+		// update
+		wip.child = reconcileChildFibers(wip, current?.child, children);
+	} else {
+		// mount
+		wip.child = mountChildFibers(wip, null, children);
+	}
 };
