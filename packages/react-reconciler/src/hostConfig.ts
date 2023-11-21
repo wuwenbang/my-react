fdce9daf3e5a0f1af9e3fa60b11b5eac67d0bf24
wuwenbang <wuwenbang@foxmail.com>
2023-11-21 16:17:42 +0800

    feat: hostConfig

--- packages/react-reconciler/src/hostConfig.ts
+++ packages/react-reconciler/src/hostConfig.ts
@@ -1,1 +1,13 @@
 export type Container = any;
+
+export const createInstance = (...args: any) => {
+	return args;
+};
+
+export const createTextInstance = (...args: any) => {
+	return args;
+};
+
+export const appendInitialChild = (...args: any) => {
+	return args;
+};
