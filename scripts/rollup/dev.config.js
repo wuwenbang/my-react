fc80254297fa145a0f88aa38e837827668531861
wuwenbang <wuwenbang@foxmail.com>
2023-11-27 17:31:06 +0800

    feat: config update

---
+++ scripts/rollup/dev.config.js
@@ -0,0 +1,4 @@
+import reactConfig from './react.config';
+import reactDomConfig from './react-dom.config';
+
+export default () => [...reactConfig, ...reactDomConfig];
--- scripts/rollup/utils.js
+++ scripts/rollup/utils.js
@@ -22,7 +22,8 @@ export function getPackageJSON(pkgName) {
 
 export function getBaseRollupPlugins({
 	alias = {
-		__DEV__: true
+		__DEV__: true,
+		preventAssignment: true
 	},
 	tsconfig = {}
 } = {}) {
